7adca0798636ba17ccee1f46aded0aba68426e55
Renata Reinartz <rcreinartz@gmail.com>
2023-02-07 15:11:21 -0700

    error className

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Header= () => {
     return (
         <div className='hero'>
-            <div class="heroInner">
+            <div className="heroInner">
                 <h1 className='heroHeadline'>Get Glossy </h1>
-                <h2 class="h2Title">Treat Yourself </h2>
-                <h3 class="subtitle">30% off select lipstick products</h3>
+                <h2 className="h2Title">Treat Yourself </h2>
+                <h3 className="subtitle">30% off select lipstick products</h3>
                 <p className='subtitle'>For a limited time</p>
                 
             </div>
